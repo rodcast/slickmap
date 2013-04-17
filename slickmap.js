a72962340055fcda7aaa4bc4e1018b40373997fb
Rodrigo Castilho <rodcast@gmail.com>
2013-04-17 15:44:30 -0300

    Refactored

--- slickmap.js
+++ slickmap.js
@@ -1,4 +1,4 @@
-/*
+/*!
  slickmap v 0.1
  Developed by Rodrigo Castilho
  email: rodcast@gmail.com
@@ -11,72 +11,70 @@
  
  @Inspired on SlickMap CSS:
    http://astuteo.com/slickmap/
-*/
-var Slickmap = (function(d) {
-    String.prototype.stripId = function() {
-        return this.toLowerCase().replace(/^\s*|\s$/g, "").replace(/\s+/g, "_").replace(/[áàâãäª]/g, "a").replace(/[éèêë]/g, "e").replace(/[íìîï]/g, "i").replace(/[óòôõöº]/g, "o").replace(/[úùûü]/g, "u").replace(/ç/g, "c").replace(/\W/g, "_").replace(/_+/g, "_");
-    };
+ */
+var Slickmap = Slickmap || (function(d) {
+  "use strict";
+  
+  String.prototype.stripId = function() {
+    return this.toLowerCase().replace(/^\s*|\s$/g, "").replace(/\s+/g, "_").replace(/[áàâãäª]/g, "a").replace(/[éèêë]/g, "e").replace(/[íìîï]/g, "i").replace(/[óòôõöº]/g, "o").replace(/[úùûü]/g, "u").replace(/ç/g, "c").replace(/\W/g, "_").replace(/_+/g, "_");
+  };
 
-    function getMap() {
-        if (typeof (_slickmap) !== "undefined") {
-            try {
-                var 
-                    sitemap = d.getElementById("sitemap"),
-                    h1 = d.createElement("h1"),
-                    h2 = d.createElement("h2");
+  function getMap() {
+    if (_slickmap instanceof Object && _slickmap.hasOwnProperty("items")) {
+      try {
+        var sitemap = d.getElementById("sitemap"),
+            h1 = d.createElement("h1"),
+            h2 = d.createElement("h2");
 
-                sitemap.className = "sitemap";
+        sitemap.className = "sitemap";
 
-                h1.innerHTML = _slickmap.title || "";
-                h2.innerHTML = _slickmap.subtitle || "";
+        h1.innerHTML = _slickmap.title || "";
+        h2.innerHTML = _slickmap.subtitle || "";
 
-                sitemap.appendChild(h1).parentNode.appendChild(h2);
+        sitemap.appendChild(h1).parentNode.appendChild(h2);
 
-                var mapTree = function(map) {
-                    var 
-                        i = 0,
-                        l = map.item.length,
-                        items, a, li,
-                        ul = d.createElement("ul");
+        var mapTree = function(map) {
+          var i = 0,
+              l = map.item.length,
+              items, a, li,
+              ul = d.createElement("ul");
 
-                    for (; i < l; i++) {
-                        li = d.createElement("li");
-                        a = d.createElement("a");
+          for (; i < l; i++) {
+            li = d.createElement("li");
+            a = d.createElement("a");
 
-                        items = map.item[i];
+            items = map.item[i];
 
-                        a.appendChild(d.createTextNode(items.name));
-                        a.href = items.link;
-                        a.title = items.description;
+            a.appendChild(d.createTextNode(items.name));
+            a.href = items.link;
+            a.title = items.description;
 
-                        li.id = (items.name).stripId();
-                        li.appendChild(a);
+            li.id = (items.name).stripId();
+            li.appendChild(a);
 
-                        if (items.item) {
-                            li.appendChild(arguments.callee(items));
-                        }
-
-                        ul.appendChild(li);
-                    }
-                    return ul;
-                };
-
-                if (_slickmap.hasOwnProperty("items")) {
-                    sitemap.appendChild(mapTree(_slickmap.items[0]));
-                    sitemap.getElementsByTagName("ul")[0].id = "primaryNav";
-
-                    //Fixed position Site map
-                    d.getElementById("primaryNav").style.marginTop = "30px";
-                }
-            } catch (e) {
-                alert(e);
+            if (items.item) {
+              li.appendChild(arguments.callee(items));
             }
-        }
-    }
 
-    return {
-        init: function() {
-            getMap();
+            ul.appendChild(li);
+          }
+          
+          return ul;
+        };
+
+        if (_slickmap.hasOwnProperty("items")) {
+          sitemap.appendChild(mapTree(_slickmap.items[0]));
+          sitemap.getElementsByTagName("ul")[0].id = "primaryNav";
         }
-    };
+      } catch (e) {
+        alert(e);
+      }
+    }
+  }
+
+  return {
+    init: function() {
+      getMap();
+    }
+  };
 }(document));
